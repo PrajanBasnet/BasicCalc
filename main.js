1cb4e029c669c9c3d8cef56bd3db73f5a31554a9
PrajwalBnet <spiderbasnet140@gmail.com>
2024-06-27 09:26:41 +0545

    working calc

--- main.js
+++ main.js
@@ -1,2 +1,61 @@
 const buttons = document.querySelectorAll(".btn");
 const output = document.querySelector(".output");
+let count = "";
+let flag = 0;
+buttons.forEach((buttons) => {
+  buttons.addEventListener("click", handleNumber);
+});
+
+function handleNumber() {
+  let num = this.getAttribute("mydata");
+
+  count += num;
+
+
+    if(count.includes("ac")){
+        clear();
+    }
+    if(count.includes("x")){
+        backSpace();
+    }
+  if (count.includes("++") || count.includes("--") || count.includes("//")) {
+    count = count.slice(0, -1);
+    console.log("plus plus " + count);
+    result(count);
+  }
+  if (flag > 0) {
+    count = count.replace("=", "");
+    flag = 0;
+  }
+  console.log(count);
+  result(count);
+}
+
+function result(number) {
+
+  if (number.includes("=")) {
+    let newNum = number.replace("=", "");
+    if (newNum.lastIndexOf(/[\+\-/]/.test(newNum))) {
+      output.innerHTML = `${eval(newNum)}`;
+      console.log("woo" + newNum);
+      flag += 1;
+      count = "";
+    } else {
+      output.textContent = number;
+    }
+  }else{
+    output.textContent = number;
+  }
+}
+
+function clear(){
+    output.textContent = "empty";
+   count = "";
+}
+
+function backSpace(){
+     count = count.slice(0,-1);
+     count = count.slice(0,-1);
+
+    console.log(count);
+}
